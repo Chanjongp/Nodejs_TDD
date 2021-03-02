e4df0d177d84dc20ad30761862cefd4e58316557
DESKTOP-GJBDRJT\s9420 <pcj0198@daum.net>
2021-03-02 18:29:25 +0900

    Practice

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  // res.end('Hello World!\n');
-  // console.log(req.url);
-  if(req.url === '/'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-  } else if(req.url === '/users') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('User List');
-  }
+function commonmw(req, res, next) {
+  console.log('commonmw');
+  next(new Error('error ouccered'));
+}
 
-  else {
-    res.statusCode = 400;
-    res.end('Not Found');
-  }
-});
+function errormw(err, req, res, next) {
+  console.log(err.message);
+  next();
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(commonmw);
+app.use(errormw);
+
+
+function logger(req, res, next){
+  console.log('i am logger');
+  next();
+}
+
+function logger2(req, res, next) {
+  console.log('i am logger2');
+  next();
+}
+
+app.use(logger);
+app.use(logger2);
+app.use(morgan('dev'));
+
+app.listen(3000, function() {
+  console.log('Server is running');
+})
